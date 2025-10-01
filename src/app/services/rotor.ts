7020009614ce6f548f183a118ffc4e38d63c7fa4
parsalemi <parsalemi@outlook.com>
2025-10-01 20:25:59 +0330

    refactor: remove duplicate and unwanted codes

--- src/app/services/rotor.ts
+++ src/app/services/rotor.ts
@@ -12,7 +12,11 @@ export class Rotor {
   rotors$ = this._rotors$.asObservable();
 
   private _original: RotorsModel | null = null;
-  env = environment
+  env = environment;
+
+  private shift(rotor: string, n: number) {
+    return rotor.slice(n) + rotor.slice(0, n);
+  }
 
   async loadRotors(): Promise<RotorsModel> {
     if (!this._original) {
@@ -22,7 +26,7 @@ export class Rotor {
       this._original = rotors;
       this._rotors$.next(rotors);
     } else if (!this._rotors$.value) {
-      this._rotors$.next({...this._original});
+      this._rotors$.next({ ...this._original });
     }
     return this._rotors$.value!;
   }
@@ -31,56 +35,40 @@ export class Rotor {
     return this._rotors$.value;
   }
 
-  async setRotorSetting(settings: number[]){
+  async setRotorSetting(settings: number[]) {
     await this.loadRotors();
-    if(!this._original) return;
+    if (!this._original) return;
 
-    const shift = (rotor: string, n: number): string => rotor.slice(n) + rotor.slice(0, n);
-    let [s1, s2, s3] = settings;
-    const rotor1 = shift(this._original.rotor1, s1 % this._original.rotor1.length);
-    const rotor2 = shift(this._original.rotor2, s2 % this._original.rotor2.length);
-    const rotor3 = shift(this._original.rotor3, s3 % this._original.rotor3.length);
-
-    this._rotors$.next({rotor1, rotor2, rotor3});
+    const [s1, s2, s3] = settings;
+    this._rotors$.next({
+      rotor1: this.shift(this._original.rotor1, s1 % this._original.rotor1.length),
+      rotor2: this.shift(this._original.rotor2, s2 % this._original.rotor2.length),
+      rotor3: this.shift(this._original.rotor3, s3 % this._original.rotor3.length),
+    });
   }
 
   async rotateRotors(step: number) {
     const current = this.getRotorSnapshot();
-    if(!current) return;
+    if (!current) return;
 
     let { rotor1: r1, rotor2: r2, rotor3: r3 } = current;
 
     r1 = r1.slice(1) + r1[0];
-    if(step % 53 === 0) {
-      r2 = r2.slice(1) + r2[0];
-    }
-    if(step % (53 * 53) === 0) {
-      r3 = r3.slice(1) + r3[0];
-    }
-    this._rotors$.next({rotor1: r1, rotor2: r2, rotor3: r3});
-  }
-  
-  async resetRotor(settings: number[]){
-    if(!this._original) {
-      await this.loadRotors();
-      return;
-    }
-    const rotors = await firstValueFrom(
-      this._http.get<RotorsModel>(`${this.env.fileUrl}/shuffle.json`)
-    );
-    this._rotors$.next(rotors);
-    await this.setRotorSetting(settings)
+    if (step % 53 === 0) r2 = r2.slice(1) + r2[0];
+    if (step % (53 * 53) === 0) r3 = r3.slice(1) + r3[0];
+    
+    this._rotors$.next({ rotor1: r1, rotor2: r2, rotor3: r3 });
   }
 
-  async resetRotorsWithSettings(settings: number[]) {
-    await this.loadRotors();
-    await this.resetRotor(settings);
+  async resetRotor(settings: number[]) {
+    if (!this._original) await this.loadRotors();
+    this._rotors$.next({ ...this._original! });
     await this.setRotorSetting(settings);
   }
 
   getReflector(c: string) {
-    const alphabets = 'abcdefghijklmnopqrstuvwxyz=+-_()*&^%$#@![]{};:/?.,<> ';
-    return alphabets[alphabets.length - alphabets.indexOf(c) - 1];
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz=+-_()*&^%$#@![]{};:/?.,<> ';
+    return alphabet[alphabet.length - alphabet.indexOf(c) - 1];
   }
 }
 
