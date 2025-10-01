bc3befb89a830d9b0b61f85d2b628a8416b1cab2
parsalemi <parsalemi@outlook.com>
2025-10-01 20:11:54 +0330

    add: httpClient to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
