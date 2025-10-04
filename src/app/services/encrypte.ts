dcbb566eca27d063bcca6333bc4a7ed0f73ca176
parsalemi <parsalemi@outlook.com>
2025-10-05 02:16:53 +0330

    add: farsi language

--- src/app/services/encrypte.ts
+++ src/app/services/encrypte.ts
@@ -7,24 +7,31 @@ import { firstValueFrom } from 'rxjs';
 })
 export class Encrypte {
   private _rotorService = inject(Rotor);
-  private _alphabet = 'abcdefghijklmnopqrstuvwxyz=+-_()*&^%$#@![]{};:/?.,<> ';
+  
+  private get _alphabet() {
+    return this._rotorService.language === 'english' 
+    ? 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=+-_()*&^%$#@![]{};:/?.,<> ' 
+    : 'آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی=+-_()*&^%$#@![]{};:/?.,<>0123456789 ';
+  }
 
   async encrypte_one_char(c: string){
     await this._rotorService.loadRotors();
     const rotors = this._rotorService.getRotorSnapshot(); 
     if(!rotors) return
-    let index = this._alphabet.indexOf(c);
+    const alphabet = this._alphabet;
+
+    let index = alphabet.indexOf(c);
     if(index === -1) return c;
 
     const { rotor1: r1, rotor2: r2, rotor3: r3} = rotors;
     let c1, c2, c3, reflected;
-    c1 = r1[this._alphabet.indexOf(c)];
-    c2 = r2[this._alphabet.indexOf(c1)];
-    c3 = r3[this._alphabet.indexOf(c2)];
+    c1 = r1[alphabet.indexOf(c)];
+    c2 = r2[alphabet.indexOf(c1)];
+    c3 = r3[alphabet.indexOf(c2)];
     reflected = this._rotorService.getReflector(c3);
-    c3 = this._alphabet[r3.indexOf(reflected)];
-    c2 = this._alphabet[r2.indexOf(c3)];
-    c1 = this._alphabet[r1.indexOf(c2)];
+    c3 = alphabet[r3.indexOf(reflected)];
+    c2 = alphabet[r2.indexOf(c3)];
+    c1 = alphabet[r1.indexOf(c2)];
     return c1;
   }
 
