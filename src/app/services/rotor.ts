ccdf7c2e923eb96bd6d2d6fd08f0f9acfa6b9ac9
parsalemi <parsalemi@outlook.com>
2025-10-05 02:16:21 +0330

    add: language handler methods

--- src/app/services/rotor.ts
+++ src/app/services/rotor.ts
@@ -8,11 +8,25 @@ import { environment } from '../../environments/environment';
 })
 export class Rotor {
   private _http = inject(HttpClient);
+  env = environment;
+
   private _rotors$ = new BehaviorSubject<RotorsModel | null>(null);
   rotors$ = this._rotors$.asObservable();
 
+  private _language = new BehaviorSubject<'english' | 'farsi'>('english');
+  language$ = this._language.asObservable();
+
   private _original: RotorsModel | null = null;
-  env = environment;
+
+  setLanguage(lang: 'english' | 'farsi') {
+    this._language.next(lang);
+    this._original = null;
+    this._rotors$.next(null);
+  }
+  
+  get language() {
+    return this._language.value;
+  }
 
   private shift(rotor: string, n: number) {
     return rotor.slice(n) + rotor.slice(0, n);
@@ -20,8 +34,10 @@ export class Rotor {
 
   async loadRotors(): Promise<RotorsModel> {
     if (!this._original) {
+      const filename = this.language === 'farsi' ? 'farsi-shuffle.json' : 'shuffle.json';
+      console.log(filename, this.language);
       const rotors = await firstValueFrom(
-        this._http.get<RotorsModel>(`${this.env.fileUrl}/shuffle.json`)
+        this._http.get<RotorsModel>(`${this.env.fileUrl}/${filename}`)
       );
       this._original = rotors;
       this._rotors$.next(rotors);
@@ -52,11 +68,12 @@ export class Rotor {
     if (!current) return;
 
     let { rotor1: r1, rotor2: r2, rotor3: r3 } = current;
+    const length = r1.length;
 
     r1 = r1.slice(1) + r1[0];
-    if (step % 53 === 0) r2 = r2.slice(1) + r2[0];
-    if (step % (53 * 53) === 0) r3 = r3.slice(1) + r3[0];
-    
+    if (step % length === 0) r2 = r2.slice(1) + r2[0];
+    if (step % (length * length) === 0) r3 = r3.slice(1) + r3[0];
+
     this._rotors$.next({ rotor1: r1, rotor2: r2, rotor3: r3 });
   }
 
@@ -67,7 +84,9 @@ export class Rotor {
   }
 
   getReflector(c: string) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz=+-_()*&^%$#@![]{};:/?.,<> ';
+    const english = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=+-_()*&^%$#@![]{};:/?.,<> ';
+    const farsi = 'آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی=+-_()*&^%$#@![]{};:/?.,<>0123456789 '
+    const alphabet = this.language === 'farsi' ? farsi : english;
     return alphabet[alphabet.length - alphabet.indexOf(c) - 1];
   }
 }
