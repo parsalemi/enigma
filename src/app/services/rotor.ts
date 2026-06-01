c0b0fa7f72cf4aaaadaadb0e4d62791588f1b04e
parsalemi <parsalemi@outlook.com>
2026-06-01 10:15:12 +0330

    fix: file path

--- src/app/services/rotor.ts
+++ src/app/services/rotor.ts
@@ -37,7 +37,7 @@ export class Rotor {
       const filename = this.language === 'farsi' ? 'farsi-shuffle.json' : 'shuffle.json';
       const rotors = await firstValueFrom(
         // this._http.get<RotorsModel>(`${this.env.fileUrl}/${filename}`)
-        this._http.get<RotorsModel>(`/${filename}`)
+        this._http.get<RotorsModel>(filename)
       );
       this._original = rotors;
       this._rotors$.next(rotors);
